ea85543c7dd77d04490ac75f5320ff759da6f959
joannabpark <joannabpark1@gmail.com>
2020-08-03 21:06:57 -0500

    Done

--- run.rb
+++ run.rb
@@ -7,28 +7,48 @@ spicy_foods = [
 ]
 
 def print_spicy_foods(spicy_foods)
-
+spicy_foods.each do |food_description|
+  chili_emoji = "🌶" * food_description[:heat_level].to_i
+ puts "#{food_description[:name]} (#{food_description[:cuisine]}) | Heat Level: #{chili_emoji}"
+end
 end
 
 def get_names(spicy_foods)
-
+spicy_foods.map do |food_description|
+  food_description[:name]
+end
 end
 
 def spiciest_foods(spicy_foods)
-
+  spicy_heat = spicy_foods.select do |food_description|
+  food_description[:heat_level] > 5
 end
+spicy_heat
+end 
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-
+spicy_foods.find do |food_description|
+    if cuisine == food_description[:cuisine]
+      return food_description
+    end
+  end
 end
+
+get_spicy_food_by_cuisine(spicy_foods, "American")
 
 # BONUS Deliverables
 def print_spiciest_foods(spicy_foods)
-
+ spiciest_foods = spiciest_foods(spicy_foods)
+ print_spicy_foods(spiciest_foods)
 end
 
 def average_heat_level(spicy_foods)
-
+  heat_level_array = []
+spicy_foods.map do |food_description|
+  heat_level_array << food_description[:heat_level] 
+end
+ average_heat = (heat_level_array.sum) / (heat_level_array.count)
+ average_heat
 end
 
 # Use this to test your methods
